64b05a5b4ffea0bb28e7202f7f2b2d5de65b564c
master <kmaily1122012@gmail.com>
2023-11-20 13:50:53 +0800

    <li> 。隱藏 、 新增 css使用

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/folio.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
 
 
